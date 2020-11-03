693ce7399dd8cd67d43134f81cdce8492c4e1769
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-11-03 22:21:27 +0900

    [add] update_or_create_from_rssメソッド追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  def self.update_or_create_from_rss(item)
+    find_or_initialize_by(id: item[:id]).tap do |article|
+      article.update(title: item[:title], description: item[:description], link: item[:link], thumbnail: item[:thumbnail])
+    end
+  end
 end
